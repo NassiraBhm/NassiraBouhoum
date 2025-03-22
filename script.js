d265f995cb70122ad939beb5919f7ad17594f06a
Nassira Bouhoum <nassirabouhoum@gmail.com>
2025-03-22 23:51:34 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const sidebar = document.querySelector(".sidebar");
+        const hamburger = document.createElement("button");
+        hamburger.classList.add("hamburger");
+        hamburger.innerHTML = '<div></div><div></div><div></div>';
+        document.body.appendChild(hamburger);
+
+        hamburger.addEventListener("click", function () {
+            sidebar.classList.toggle("active");
+        });
+    });
+</script>
