00b572e50fdd6300c4032ec7606e9a24e91f849b
Nassira Bouhoum <nassirabouhoum@gmail.com>
2025-03-23 15:32:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 <script>
-    document.addEventListener("DOMContentLoaded", function () {
-    const sidebar = document.querySelector(".sidebar");
-    const hamburger = document.createElement("button");
-    hamburger.classList.add("hamburger");
-    hamburger.innerHTML = '<div></div><div></div><div></div>';
-    document.body.appendChild(hamburger);
-
-    hamburger.addEventListener("click", function () {
-        sidebar.classList.toggle("active");
-    });
-});
+function toggleMenu() {
+    var sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle("active");
+}
 </script>
